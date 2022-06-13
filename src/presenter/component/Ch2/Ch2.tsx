7a7fbb3a593fd64635004ef040e99735f80d9f96
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-06-13 18:47:50 +0900

    fix

--- src/presenter/component/Ch2/Ch2.tsx
+++ src/presenter/component/Ch2/Ch2.tsx
@@ -1,7 +1,9 @@
+import { Header } from "./Header";
 import { Hero } from "./Hero";
 
 export const Ch2: React.FC = () => (
   <>
+    <Header />
     <Hero />
   </>
 );
--- src/presenter/component/Ch2/Hero/Hero.tsx
+++ src/presenter/component/Ch2/Hero/Hero.tsx
@@ -4,6 +4,7 @@ export const Hero: React.FC = () => (
   <>
     <S.Hero>
       <S.Title>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/catch.svg" alt="Take A Break" />
       </S.Title>
       <S.ArrowImg src="/arrow.svg" />
